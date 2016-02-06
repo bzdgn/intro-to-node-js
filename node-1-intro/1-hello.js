1a90cf85ae39a6807f937b56c0ba984bd1348d6a
Levent Divilioglu <divilioglu@gmail.com>
2016-02-06 18:11:18 +0000

    hello js files, simple server

--- node-1-intro/1-hello.js
+++ node-1-intro/1-hello.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+var http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World Lan\n');
+}).listen(process.env.PORT, process.env.IP);
+
+console.log('Server running');
